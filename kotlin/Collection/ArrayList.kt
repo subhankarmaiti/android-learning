a7bd4803f0d5b37384187afcdefe15807c689a89
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-07-17 11:54:29 +0530

    Array List

---
+++ kotlin/Collection/ArrayList.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Pets App")
+    print("Enter number of Pets: ")
+    val maxSize = readLine()!!.toInt()
+    var listOfPets = ArrayList<String>()
+    for(i in 0 until maxSize) {
+        listOfPets.add(readLine()!!.toString())
+    }
+    // pets are using index
+    for(i in 0 until listOfPets.size) {
+        println("Pet $i: ${listOfPets[i]}")
+    }
+    // pets are using object
+    for(pet in listOfPets) {
+        println("Pet: $pet")
+    }
+    
+}
