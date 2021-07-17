ac477741aa2d283aa23a5db13c915b35d6f03f84
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-07-17 22:16:10 +0530

    Polymorphism

---
+++ kotlin/Function/Polymorphism.kt
@@ -0,0 +1,12 @@
+fun showInfo(cardID:Int) {
+    println("CardID: $cardID")
+}
+
+fun showInfo(name:String) {
+    println("Name: $name")
+}
+
+fun main() {
+    showInfo("Subhankar")
+    showInfo(735363)
+}
