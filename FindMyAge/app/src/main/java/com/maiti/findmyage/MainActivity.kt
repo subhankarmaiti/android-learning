d52563bf6cee78a600d97e66adebc78551539370
Subhankar Maiti <subhankaraiti40@gmail.com>
2021-07-19 22:55:00 +0530

    Find My Age completed

--- FindMyAge/app/src/main/java/com/maiti/findmyage/MainActivity.kt
+++ FindMyAge/app/src/main/java/com/maiti/findmyage/MainActivity.kt
@@ -2,10 +2,31 @@ package com.maiti.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val getAgeButton = findViewById<Button>(R.id.buttonAge)
+        getAgeButton.setOnClickListener{
+            onClickGetAge()
+        }
+
+    }
+
+    private fun onClickGetAge() {
+
+        val ageInput = findViewById<EditText>(R.id.edDOB)
+        val tvShowAge = findViewById<TextView>(R.id.tvShowAge)
+        val userDOB = Integer.parseInt(ageInput.text.toString())
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val userAgeInYears = currentYear - userDOB
+        tvShowAge.text = "Your Age is $userAgeInYears Years"
+        Log.d("LogsMe","Your Age is $userAgeInYears Years")
     }
 }
